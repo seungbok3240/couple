610dba2fbea06dbdfe94217dd6db8ca38d7f81c8
seungboklee <seungbok.lee@villanema.com>
2024-02-25 16:28:16 +0900

    Init

---
+++ src/main/java/com/example/couple/CoupleApplication.java
@@ -0,0 +1,13 @@
+package com.example.couple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/couple/CoupleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.couple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
